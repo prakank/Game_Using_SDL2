59f1ba31e1691737d1be6462e364e95c0f12a9a6
Prakhar Aggarwal <prakharaggarwal87@gmail.com>
2021-04-27 02:20:20 +0530

    Organised in Folder

---
+++ ECS/Components.hpp
@@ -0,0 +1,8 @@
+#ifndef COMPONENT_HPP
+#define COMPONENT_HPP
+
+#include "EntityComponentSystem.hpp"
+#include "PositionComponents.hpp"
+#include "SpriteComponent.hpp"
+
+#endif
---
+++ ECS/SpriteComponent.hpp
@@ -0,0 +1,10 @@
+#ifndef SPRITE_COMPONENT
+#define SPRITE_COMPONENT
+
+#include "EntityComponentSystem.hpp"
+
+class SpriteComponent : public Component{
+
+};
+
+#endif
